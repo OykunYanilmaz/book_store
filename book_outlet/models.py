9c1a8f78ad646f41b63abf94b437e4ca3e97e760
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-10 00:46:29 +0300

    Adding one to many relation

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -3,12 +3,25 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
-    author = models.CharField(null=True, max_length=100)
+    # author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", blank=True, null=False, db_index=True) # , editable=False
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)  # , editable=False
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
@@ -19,4 +32,3 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
-
