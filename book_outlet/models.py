8d9b87c039fac3f97bbfdfc048bdea35f5e346d1
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-10 01:39:16 +0300

    Adding many to many relation

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,6 +4,17 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
 class Address(models.Model):
     street = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=5)
@@ -11,7 +22,7 @@ class Address(models.Model):
 
     def __str__(self):
         return f"{self.street}, {self.postal_code}, {self.city}"
-    
+
     class Meta:
         verbose_name_plural = "Address Entries"
 
@@ -35,6 +46,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)  # , editable=False
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
